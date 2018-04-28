333f80c3b9aae11faf50072d3fc58c93ee831da8
Paul Borawski <paulborawski14@gmail.com>
2018-04-28 13:03:32 -0700

    require authRoutes and app app to the function immediately

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 require('./services/passport');
-const authRoutes = require('./routes/authRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-authRoutes(app);
+require('./routes/authRoutes')(app);
 
 app.get('/', (req,res) => {
   res.send('running')
