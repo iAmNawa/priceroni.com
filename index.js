805d043e3b0c37690c1f92db3659984f06941b60
Paul Borawski <paulborawski14@gmail.com>
2018-04-28 16:32:00 -0700

    added mongoose

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(keys.mongoURI);
 
 require('./routes/authRoutes')(app);
 
