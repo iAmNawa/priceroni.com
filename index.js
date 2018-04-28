0f1bbe7cdbec10141b107e931fec3b084df926f1
Paul Borawski <paulborawski14@gmail.com>
2018-04-28 10:03:04 -0700

    adding keys.js to gitignore

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const PORT = process.env.PORT || 5000;
 
-// route handler
-app.get('/', (req, res) => {
-  res.send({ hi: 'there paul' });
-});
+
+passport.use(new GoogleStrategy());
 
 app.listen(PORT, function() {
   console.log('server is running on', PORT)
