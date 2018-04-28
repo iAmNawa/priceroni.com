90d64a072a9229e9d69896552620320d52a3d60e
Paul Borawski <paulborawski14@gmail.com>
2018-04-28 11:22:54 -0700

    /auth/google is redirecting to google correctly

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require('express');
 const app = express();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const PORT = process.env.PORT || 5000;
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
-passport.use(new GoogleStrategy());
+app.get(
+  '/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/', (req,res) => {
+  res.send('running')
+})
 
 app.listen(PORT, function() {
   console.log('server is running on', PORT)
