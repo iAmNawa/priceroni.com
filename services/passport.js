150a10566cc92db65e69370715d54ad7967baf5b
Paul Borawski <paulborawski14@gmail.com>
2018-05-13 14:14:03 -0700

    refactored code to get rid of else statement.  if existingUser return done

--- services/passport.js
+++ services/passport.js
@@ -27,13 +27,10 @@ passport.use(
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleId: profile.id })
         if (existingUser) {
-          // we already have a record with the given profile Id
-          done(null, existingUser);
-        } else {
-          // we don't have a record Id, make a new record
-          const user = await new User({ googleId: profile.id }).save()
-            done(null, user);
+          return done(null, existingUser);
         }
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user);
     }
   )
 );
