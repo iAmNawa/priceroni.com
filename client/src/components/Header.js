10ae4822c35bf93f9844cfee346c4ca1da63fe43
Paul Borawski <paulborawski14@gmail.com>
2018-05-15 09:11:00 -0700

    navbar showing login with google and showing logout when logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login with Google</a></li>
+        );
+      default:
+        return <li><a>Logout</a></li>;
+    }
+  }
+
   render() {
-    console.log(this.props)
     return (
       <nav>
         <div className="nav-wrapper">
@@ -11,9 +23,7 @@ class Header extends Component {
             Email
           </a>
           <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
