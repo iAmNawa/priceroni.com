1d941d41cf80d875454f131a34bc3535219f4b8d
Paul Borawski <paulborawski14@gmail.com>
2018-05-22 10:06:18 -0700

    added stripe and body-parser middleware

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,3 +1,8 @@
-module.exports = app => {
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
 
+module.exports = app => {
+  app.post('/api/stripe', (req, res) => {
+
+  });
 };
